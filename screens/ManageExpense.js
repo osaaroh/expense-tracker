a97015851f15c29433ab63fc95e3b098349c8e76
Osaro Iyoha <saro.iyoha@gmail.com>
2024-03-03 18:37:48 +0100

    Starter Code

---
+++ screens/ManageExpense.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+function ManageExpense() {
+  return (
+    <Text>ManageExpense</Text>
+  )
+}
+
+export default ManageExpense
---
+++ screens/RecentExpenses.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+function RecentExpenses() {
+  return (
+    <Text>RecentExpenses</Text>
+  )
+}
+
+export default RecentExpenses
