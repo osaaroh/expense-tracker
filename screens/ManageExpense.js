527356fa915bd56373f368f001cc19cafc1bc13a
Osaro Iyoha <saro.iyoha@gmail.com>
2024-03-05 11:01:53 +0100

    Add different edit modes on one screen

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,10 +1,70 @@
-import React from 'react'
-import { Text } from 'react-native'
+import React, { useLayoutEffect } from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import IconButton from '../components/UI/IconButton';
+import { GlobalStyles } from '../constants/styles';
+import Button from '../components/UI/Button';
 
-function ManageExpense() {
+function ManageExpense({route, navigation}) {
+  const editedExpenseId = route.params?.expenseId;
+  const isEditing = !!editedExpenseId;
+
+  useLayoutEffect(()=>{
+    navigation.setOptions({
+      title: isEditing ? 'Edit Expense' : 'Add Expense'
+    })
+  }, [navigation, isEditing])
+
+  function deleteExpenseHandler(params) {
+    
+  }
+  function cancelHandler(params) {
+    
+  }
+  function confirmHandler(params) {
+    
+  }
+
   return (
-    <Text>ManageExpense</Text>
+    <View style={styles.container}>
+      <View style={styles.buttonContainer}>
+        <Button style={styles.button} mode='flat' onPress={cancelHandler}>Cancel</Button>
+        <Button style={styles.button} onPress={confirmHandler}> {isEditing ? 'Update': 'Add'}</Button>
+      </View>
+      {isEditing && (
+      <View style={styles.deleteContainer}>
+        <IconButton 
+        icon="trash" 
+        color={GlobalStyles.colors.error500}
+        size={36}
+        onPress={deleteExpenseHandler}    
+        />
+      </View>)}
+    </View>
   )
 }
 
-export default ManageExpense
+export default ManageExpense;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: GlobalStyles.colors.primary800
+  },
+  button:{
+    minWidth: 120,
+    marginHorizontal: 8
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  deleteContainer: {
+    marginTop: 16,
+    paddingTop: 8,
+    borderTopWidth: 2,
+    borderTopColor:GlobalStyles.colors.primary200,
+    alignItems: 'center'
+  }
+})
